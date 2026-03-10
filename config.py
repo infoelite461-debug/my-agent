f6654cc0e5673938ba5f1b470abf058530e6881d
infoelite461-debug <infoelite461@gmail.com>
2026-03-10 17:20:00 +0300

    Create config.py with API keys, platform settings, and content templates for the social media agent.

---
+++ config.py
@@ -0,0 +1,22 @@
+# Configuration for Social Media Agent
+
+# API Keys
+API_KEYS = {
+    'twitter': 'YOUR_TWITTER_API_KEY',
+    'facebook': 'YOUR_FACEBOOK_API_KEY',
+    'instagram': 'YOUR_INSTAGRAM_API_KEY'
+}
+
+# Platform Settings
+PLATFORM_SETTINGS = {
+    'time_zone': 'UTC',
+    'language': 'en',
+    'default_post_length': 280
+}
+
+# Content Templates
+CONTENT_TEMPLATES = {
+    'welcome_message': 'Welcome to our social media channels! We are glad to have you here.',
+    'goodbye_message': 'Thank you for being with us! See you next time!',
+    'error_message': 'Oops! Something went wrong. Please try again later.'
+}
